a015610922ae771fd18a92cd9519093ca2d366b5
Anas Mazouni <madadj4@gmail.com>
2025-02-16 16:28:57 +0100

    fix: badge appearing on top video player

--- apps/extension/src/contents/user-card.tsx
+++ apps/extension/src/contents/user-card.tsx
@@ -67,7 +67,6 @@ const FollowIcon = () => {
 export default function FollowText() {
   const { isFollower: followsYou, followingSince: followDate } = useFollowsYou(selectedUser)
   if (!followsYou) return null
-  
   return <div className="flex mt-0.5 items-center gap-2 text-white font-inter font-feature-default antialiased">
     <FollowIcon />
     <div className="text-[13px] leading-6">Follows you since {format(followDate, "MMMM d, yyyy")}</div>
